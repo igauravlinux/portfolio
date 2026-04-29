a5b121eac311c992e89718b0256123ed578c2432
gaurav <171420238+igauravraut@users.noreply.github.com>
2026-04-29 22:35:08 +0530

    initial-commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function showSidebar(){
+    const sidebar = document.querySelector('ul');
+
+    sidebar.style.display = 'flex'
+}
+function hideSidebar(){
+    const sidebar = document.querySelector('ul');
+
+    sidebar.style.display = 'none'
+}
